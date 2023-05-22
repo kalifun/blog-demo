05b3680d5b1c1a0b462aafdb5f5448ce7d8ebce7
kalifun <kalifun@163.com>
2023-05-22 23:11:52 +0800

    feat: create fun

--- models/src/tag.rs
+++ models/src/tag.rs
@@ -6,11 +6,26 @@ use crate::schema::tags;
 
 use uuid::Uuid;
 
-#[derive(Queryable)]
+#[derive(Queryable, Insertable)]
 #[diesel(table_name = tags)]
 pub struct Tag {
     pub id: Uuid,
     pub name: String,
     pub create_time: SystemTime,
     pub update_time: SystemTime,
+}
+
+pub fn create_tag(conn: &mut PgConnection, tag_name: &str) -> Tag {
+    let now = SystemTime::now();
+    let uid = Uuid::new_v4();
+    let tag_data = Tag {
+        id: uid,
+        name: tag_name.to_string(),
+        create_time: now,
+        update_time: now,
+    };
+    diesel::insert_into(tags::table)
+        .values(&tag_data)
+        .get_result(conn)
+        .expect("Error create tag failed")
 }
--- models/src/user.rs
+++ models/src/user.rs
@@ -6,11 +6,26 @@ use crate::schema::users;
 
 use uuid::Uuid;
 
-#[derive(Queryable)]
+#[derive(Queryable, Insertable, Selectable, PartialEq)]
 #[diesel(table_name = users)]
 pub struct User {
     pub id: Uuid,
     pub name: String,
     pub create_time: SystemTime,
     pub update_time: SystemTime,
+}
+
+pub fn create_user(conn: &mut PgConnection, user_name: &str) -> User {
+    let uid = Uuid::new_v4();
+    let now = SystemTime::now();
+    let user = User {
+        id: uid,
+        name: user_name.to_string(),
+        create_time: now,
+        update_time: now,
+    };
+    diesel::insert_into(users::table)
+        .values(&user)
+        .get_result(conn)
+        .expect("Error create failed")
 }
